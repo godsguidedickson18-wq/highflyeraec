4b6d5f939d31e2d1131eaf170bf51715463db6bc
godsguidedickson18-wq <godsguidedickson18@gmail.com>
2026-05-10 13:16:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   Offline      → offline.html   (friendly fallback)
  */
 
-const CACHE_VERSION  = 'v3';
+const CACHE_VERSION  = 'v4';
 const STATIC_CACHE   = `highflyer-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE  = `highflyer-dynamic-${CACHE_VERSION}`;
 const IMAGE_CACHE    = `highflyer-images-${CACHE_VERSION}`;
